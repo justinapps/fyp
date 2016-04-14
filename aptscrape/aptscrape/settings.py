f383342b83497774fcd95e8b974c811c002888e8
Justin <justinasulevicius@gmail.com>
2016-04-14 20:04:30 +0100

    Superuser, admindocs
    Created a superuser and installed admindocs

--- aptscrape/aptscrape/settings.py
+++ aptscrape/aptscrape/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admindocs', #django admin docs
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
